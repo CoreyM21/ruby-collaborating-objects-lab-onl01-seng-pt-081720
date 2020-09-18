24add25ba84478cae39161a15d2862354bfc6088
Corey Martin <coreyjmartin2@gmail.com>
2020-09-18 21:24:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,10 @@ class Song
     song
   end
   
-  def self
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
 
 end
